75b475aa02370df10a9903f95c76faf1162239ee
DavsonMelo <davsonmelo@protonmail.com>
2025-08-18 01:15:16 -0300

    76 - Criando pagina search

---
+++ src/app/game/search/[title]/page.tsx
@@ -0,0 +1,41 @@
+import { GameProps } from '@/utils/types/game';
+import { GameCard } from '@/components/GameCard';
+import { Container } from '@/components/container';
+import { Input } from '@/components/input';
+
+interface SearchPageParams { title: string }
+interface SearchPageProps { params: Promise<SearchPageParams> }
+
+
+async function getData(title: string) {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_API_URL}/next-api/?api=game&title=${title}`
+    );
+    return res.json();
+  } catch (err) {
+    return null;
+  }
+}
+
+export default async function Search({ params }: SearchPageProps) {
+  const { title } = await params
+  const games: GameProps[] = await getData(title);
+
+  return (
+    <main className="w-full text-black">
+      <Container>
+        <Input />
+        <h1 className="font-bold text-xl mt-8 mb-5">
+          Veja o que encontramos na nossa base:
+        </h1>
+
+        {!games && <p>Esse jogo não foi encontrado! ...</p>}
+
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
+          {games && games.map((item) => <GameCard key={item.id} data={item} />)}
+        </section>
+      </Container>
+    </main>
+  );
+}
